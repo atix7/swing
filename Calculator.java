459d75d3acfdd6817d07e91b2a52cf21ebbf8678
atix7 <46376360+atix7@users.noreply.github.com>
2023-03-20 16:11:00 +0100

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,24 +2,28 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Calculator implements ActionListener {
-    static void printW() {
-        System.out.println("ww");
-    }
-
     JFrame frame;
     JTextField texfield;
     JButton[] numberButtons = new JButton[10];
-    JButton[] functionButtons = new JButton[8];
+    JButton[] functionButtons = new JButton[9];
     JButton addButton, subButton, mulButton, divButton;
-    JButton decButton, equButton, delButton, clrButton;
+    JButton decButton, equButton, delButton, clrButton, negButton;
     JPanel panel;
 
     Font myFont = new Font("Bauhaus 93", Font.PLAIN, 25);
 
-    double num1 = 0, num2 = 0, result;
+    double num1;
+    double num2 = 0, result, tempResult;
     char operator;
+
+    ArrayList<String> fullList = new ArrayList<String>();  //collect the /*-+ signs
+
+    public void toFullList(Integer n) {
+        fullList.add(String.valueOf(n));
+    }
 
     //Method check 2nd "+-*." char pushed?
 
@@ -44,6 +48,8 @@ public class Calculator implements ActionListener {
         equButton = new JButton("=");
         delButton = new JButton("Delete");
         clrButton = new JButton("Clear");
+        negButton = new JButton("+-");
+
 
         functionButtons[0] = addButton;
         functionButtons[1] = subButton;
@@ -53,21 +59,25 @@ public class Calculator implements ActionListener {
         functionButtons[5] = equButton;
         functionButtons[6] = delButton;
         functionButtons[7] = clrButton;
+        functionButtons[8] = negButton;
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < functionButtons.length; i++) {
             functionButtons[i].addActionListener(this);
             functionButtons[i].setFont(myFont);
             functionButtons[i].setFocusable(false);
         }
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < numberButtons.length; i++) {
             numberButtons[i] = new JButton(String.valueOf(i));
             numberButtons[i].addActionListener(this);
             numberButtons[i].setFont(myFont);
             numberButtons[i].setFocusable(false);
         }
-        delButton.setBounds(50, 430, 145, 50);
-        clrButton.setBounds(205, 430, 145, 50);
+        delButton.setBounds(50, 430, 110, 50);
+        //delButton.setBounds(50, 430, 145, 50);
+        clrButton.setBounds(170, 430, 110, 50);
+        //clrButton.setBounds(205, 430, 145, 50);
+        negButton.setBounds(288, 430, 60, 50);
 
         panel = new JPanel();
         panel.setBounds(50, 100, 300, 300);
@@ -94,72 +104,126 @@ public class Calculator implements ActionListener {
         frame.add(panel);
         frame.add(delButton);
         frame.add(clrButton);
+        frame.add(negButton);
         frame.add(texfield);
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");  // This line gives Windows Theme
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         Calculator calc = new Calculator();
     }
 
     int countDec = 0;
-    int count = 0;
+    boolean equalWasPushed = false;
+    boolean outcome = false;
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == subButton && num1 == 0) {                  //My if
-            texfield.setText("-");
-        }
+
+
         for (int i = 0; i < 10; i++) {
-            if (e.getSource() == numberButtons[i]) {
+            if (e.getSource().equals(numberButtons[i])) {
+                if (outcome) {
+                    texfield.setText("");
+                    outcome = false;
+                }
                 texfield.setText(texfield.getText().concat(String.valueOf(i)));
+                System.out.println();
             }
         }
-        if (e.getSource() == decButton) {                               //It is ok now. You can use . only once
+        if (e.getSource().equals(decButton)) {                               //It is ok now. You can use . only once
             if (countDec < 1) {
                 texfield.setText(texfield.getText().concat("."));
                 countDec++;
-            }else {
-                texfield.getText();
+            } else {
+                countDec++;
+                System.out.println("This was already the " + countDec + " -dot!");
             }
         }
-        if (e.getSource() == addButton) {
+        if (e.getSource().equals(addButton)) {
             num1 = Double.parseDouble(texfield.getText());
+            tempResult = num1;
             operator = '+';
             texfield.setText("+");
+            num1 = num1 + tempResult;
+            System.out.println("num1 " + num1 + " num2 " + num2 + "tempResult " + tempResult);
+            tempResult = 0;
+            countDec = 0;
+            //loopVar = true;
         }
-        if (e.getSource() == subButton) {
-            count++;
-            if (count>1){
-
-            }
+        if (e.getSource().equals(subButton)) {
             num1 = Double.parseDouble(texfield.getText());
             operator = '-';
             texfield.setText("");
+            countDec = 0;
         }
-        if (e.getSource() == mulButton) {
+        if (e.getSource().equals(mulButton)) {
             num1 = Double.parseDouble(texfield.getText());
             operator = '*';
             texfield.setText("");
+            countDec = 0;
         }
-        if (e.getSource() == divButton) {
+        if (e.getSource().equals(divButton)) {
             num1 = Double.parseDouble(texfield.getText());
             operator = '/';
             texfield.setText("");
+            countDec = 0;
+        }
+        if (e.getSource().equals(negButton)) {
+            num1 = Double.parseDouble(texfield.getText());
+            operator = '+';
+            texfield.setText("-");
+            outcome = false;
+            System.out.println(String.valueOf(texfield.getText()));
         }
 
-        if (e.getSource() == equButton) {
-            num2 = Double.parseDouble(texfield.getText());
-            switch (operator) {
-                case '+' -> result = num1 + num2;
-                case '-' -> result = num1 - num2;
-                case '*' -> result = num1 * num2;
-                case '/' -> result = num1 / num2;
+        if (e.getSource().equals(equButton)) {
+            try {
+                num2 = Double.parseDouble(texfield.getText());   //Ha megnyomtad = akkor nem írhat be újabb számot az eredmény után
+                equalWasPushed = true;
+            } catch (NumberFormatException e2) {
+                System.out.println("Cannot end with an operation sign");
             }
+
+            switch (operator) {
+                case '+' -> {
+                    tempResult = 0;
+                    result = num1 + num2;
+                    countDec = 0;
+                    System.out.println("num1: " + num1 + " + num2: " + num2 + " result " + result + "\n" + "tempresult " + tempResult);
+                }
+                case '-' -> {
+                    result = num1 - num2;
+                    countDec = 0;
+                    System.out.println("num1: " + num1 + " - num2: " + num2 + " result " + result);
+                }
+                case '*' -> {
+                    result = num1 * num2;
+                    countDec = 0;
+                    System.out.println("num1: " + num1 + " * num2: " + num2 + " result " + result);
+                }
+                case '/' -> {
+                    result = num1 / num2;
+                    countDec = 0;
+                    System.out.println("num1: " + num1 + " / num2: " + num2 + " Eredmeny: " + result);
+                }
+            }
+            System.out.println("*****************************************************");
             texfield.setText(String.valueOf(result));
-            num1 = result;
+            //num1 = result;
+            outcome = true;        //Ez egy számláló. ha van eredmény akkor törölje a képet
+            countDec = 1;
         }
+        //ciklus vége
+
+
         if (e.getSource() == delButton) {                           //My if
             String string = texfield.getText();
             string = string.substring(0, string.length() - 1);
@@ -171,6 +235,8 @@ public class Calculator implements ActionListener {
             num1 = 0;
             num2 = 0;
             result = 0;
+            countDec = 0;
+            outcome = false;
         }
     }
 }
